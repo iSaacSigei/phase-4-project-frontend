182073ebbf1ad46e145f943ac576a9f23b3092cf
iSaacSigei <kiplangatisack.is@gmail.com>
2022-12-12 17:20:06 +0300

    minor changes

--- src/components/Navbar/Signup.js
+++ src/components/Navbar/Signup.js
@@ -31,7 +31,7 @@ export default function Signup({ onLogin }) {
   function handleSubmit(e) {
     e.preventDefault();
     setErrors([]);
-    fetch("/signup", {
+    fetch("https://phase-4-project-production.up.railway.app/signup", {
       method: "POST",
       headers: {
         "Access-Control-Allow-Origin": "*",
